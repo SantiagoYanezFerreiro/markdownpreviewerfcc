f59dfea128ab4dbd36fd83781f61254eff5c7b2f
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-03-25 22:32:02 +0100

    included required text for app preview

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import { marked } from "marked";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+
+  **bold text**
+  `);
   return (
     <div className="App">
       <textarea
