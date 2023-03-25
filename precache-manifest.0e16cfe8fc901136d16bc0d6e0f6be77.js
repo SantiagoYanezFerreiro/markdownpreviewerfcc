f8a1c77c68ac5b3e66d5730a15abd1eae8254bf1
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-03-25 23:16:00 +0100

    Updates

---
+++ precache-manifest.0e16cfe8fc901136d16bc0d6e0f6be77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "846fd5b41b6d4d895cee",
+    "url": "/markdownpreviewerfcc/static/css/main.b1528f3e.chunk.css"
+  },
+  {
+    "revision": "846fd5b41b6d4d895cee",
+    "url": "/markdownpreviewerfcc/static/js/main.846fd5b4.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/markdownpreviewerfcc/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "6696bcc5acbbe0cc9418",
+    "url": "/markdownpreviewerfcc/static/js/2.6696bcc5.chunk.js"
+  },
+  {
+    "revision": "c39e74b88763eb1f10df",
+    "url": "/markdownpreviewerfcc/static/js/runtime~main.c39e74b8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/markdownpreviewerfcc/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b1a3e04be6b375f65f580ef922bc83e5",
+    "url": "/markdownpreviewerfcc/index.html"
+  }
+];
