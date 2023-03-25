ec4786bbc76c35fde3887a1c46bd0885a09ef7e1
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-03-25 22:07:13 +0100

    initial setup and layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      ></textarea>
+      <div id="preview">{marked.parse(text)}</div>
+      <div>aaa</div>
     </div>
   );
 }
